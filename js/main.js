75e8cc6d9c71a8d42a8573cb959ea2d714765ff2
laxmiprabhaannaldas <annaldaslaxmiprabha@gmail.com>
2020-08-07 13:51:11 +0530

    Developed the javascript logic to get winner and looser of the game

--- js/main.js
+++ js/main.js
@@ -14,18 +14,44 @@ function play(e) {
     restart.style.display = 'inline-block';
     const playerChoice = e.target.id;
     const computerChoice = getComputerChoice();
-    console.log(playerChoice, computerChoice);
+
+    const winner = getWinner(playerChoice, computerChoice);
+    console.log(playerChoice, computerChoice, winner);
 }
 
 //get compters choice
 function getComputerChoice() {
     const rand = Math.random();
     if (rand < 0.34) {
-        return 'Rock';
+        return 'rock';
     } else if (rand <= 0.67) {
-        return 'Paper';
+        return 'paper';
     } else {
-        return 'Scissors';
+        return 'scissors';
+    }
+}
+//get winner
+function getWinner(p, c) {
+    if (p === c) {
+        return 'draw';
+    } else if (p === 'rock') {
+        if (c === 'paper') {
+            return 'computer';
+        } else {
+            return 'player';
+        }
+    } else if (p === 'paper') {
+        if (c === 'scissors') {
+            return 'computer';
+        } else {
+            return 'player';
+        }
+    } else if (p === 'scissors') {
+        if (c === 'rock') {
+            return 'computer';
+        } else {
+            return 'player';
+        }
     }
 }
 
