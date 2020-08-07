ef97c67fa77f2c0d9859c3a31744c59ae7280fea
laxmiprabhaannaldas <annaldaslaxmiprabha@gmail.com>
2020-08-07 13:28:20 +0530

    Developed the basic javascript logic for getting the input of user and Computer

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const choices = document.querySelectorAll('.choice');
+const score = document.getElementById('score');
+const result = document.getElementById('result');
+const restart = document.getElementById('restart');
+const model = document.querySelector('.model');
+
+const scoreboard = {
+    player: 0,
+    computer: 0
+}
+
+//play game
+function play(e) {
+    restart.style.display = 'inline-block';
+    const playerChoice = e.target.id;
+    const computerChoice = getComputerChoice();
+    console.log(playerChoice, computerChoice);
+}
+
+//get compters choice
+function getComputerChoice() {
+    const rand = Math.random();
+    if (rand < 0.34) {
+        return 'Rock';
+    } else if (rand <= 0.67) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}
+
+//Event Listeners
+choices.forEach(choice => choice.addEventListener('click', play));
